ae45900b1f0c1e90ae8ba04b113a98eb55e3071d
Brian Vicente <bvicente@sanctumsecops.com>
2026-08-19 14:54:11 -0400

    Move formula to Formula/ so Homebrew can discover it

---
+++ Formula/cygnet-provider.rb
@@ -0,0 +1,37 @@
+class CygnetProvider < Formula
+  desc "OpenSSL 3 provider for the Sanctum SecOps / Cygnus PKI stack"
+  homepage "https://github.com/sanctumsecopsmssp/cygnet-provider"
+  url "https://github.com/sanctumsecopsmssp/cygnet-provider/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "2090ec96e7bdcc351211da9e754b394c8392e7d5033cda4c57aeacd8eabb4b63"
+  license "Apache-2.0"
+  version "0.1.1"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "openssl@3"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+           "-DCMAKE_BUILD_TYPE=Release",
+           "-DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}"
+    system "cmake", "--build", "build"
+    lib.install "build/provider/cygnetprov.dylib"
+    (libexec/"tests").install "build/tests/provider_load_test"
+  end
+
+  def caveats
+    <<~EOS
+      The Cygnet provider module was installed to:
+        #{lib}/cygnetprov.dylib
+
+      To load it, point OPENSSL_MODULES at that directory:
+        export OPENSSL_MODULES="#{lib}"
+    EOS
+  end
+
+  test do
+    assert_predicate lib/"cygnetprov.dylib", :exist?
+    ENV["OPENSSL_MODULES"] = lib
+    system libexec/"tests/provider_load_test"
+  end
+end
---
+++ homebrew/README.md
@@ -0,0 +1,7 @@
+# Deprecated location
+
+The Homebrew formula lives at `Formula/cygnet-provider.rb` in the repository root.
+
+Homebrew only discovers formulae in the repository root, a `Formula/`
+subdirectory, or a `HomebrewFormula/` subdirectory. Files in this `homebrew/`
+directory are ignored by `brew`.
